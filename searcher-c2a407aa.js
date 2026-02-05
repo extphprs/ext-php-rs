530fac261eedcb61250fe0880e9bca8646a112cb
ptondereau <ptondereau@users.noreply.github.com>
2026-02-05 13:32:43 +0000

    Deploying to gh-pages from @ extphprs/ext-php-rs@2b6c0e019b6708ca33a69f6c2d409c6be2015aa7 🚀

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-5db2da77.js',
+                path_to_root + 'searchindex-4ecb5763.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
