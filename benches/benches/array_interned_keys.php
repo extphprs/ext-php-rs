3fe389d6e82adcc54caf26951abb2c4f896bbafe
Muhammad Elhwawshy <m.elhwawshy@gmail.com>
2026-05-28 13:15:24 +0200

    fix: remove print_r

--- benches/benches/array_interned_keys.php
+++ benches/benches/array_interned_keys.php
@@ -3,5 +3,3 @@
 declare(strict_types=1);
 
 $a = bench_array_with_interned_keys((int) $argv[1]);
-
-print_r($a);
--- benches/benches/array_str_ref_keys.php
+++ benches/benches/array_str_ref_keys.php
@@ -3,5 +3,3 @@
 declare(strict_types=1);
 
 $a = bench_array_with_str_ref_keys((int) $argv[1]);
-
-print_r($a);
